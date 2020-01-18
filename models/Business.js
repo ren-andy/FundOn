dfee50cb2bdeadaf948d60f9eaf412d848487196
David <davidjuhyung@gmail.comd>
2020-01-18 16:22:19 -0500

    database intial

---
+++ models/Business.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BusinessSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 50
+    },
+    goal: {
+        type: String,
+        minlength: 5
+    },
+    usersJoined: {
+        type: Number
+    }
+});
+
+module.exports = Business = mongoose.model("Business", BusinessSchema);
