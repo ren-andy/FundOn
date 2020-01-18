27e75f3662c897e22683a66c4594bdb817fe797b
Andy Ren <andy.ren@uwaterloo.ca>
2020-01-18 16:42:39 -0500

    Merge branch 'master' of https://github.com/ren-andy/FundOn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = 5000 || process.env.PORT;
-const config = require('./config/key')
+const User = require('./models/User');
+const Business = require('./models/Business')
+const config = require('./config/key');
+const auth = require('./middleware/auth');
+const cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || 5000;
 
 mongoose
     .connect(config.mongoURI,
@@ -12,9 +16,101 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send("heloo")
+app.get('/api/user/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
 })
+/* 
+app.get('/api/user/:id', (req, res) => {
+    User.findById(res.params.id)
+        .then(userData => {
+            res.json(userData);
+        })
+}) */
+
+app.get('/api/users/', (req, res) => {
+    User.find({})
+        .then(users => {
+            res.json(users)
+        })
+})
+
+app.post('/api/user/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if (err) {
+            return res.json({ success: false, err })
+        } else {
+            return res.status(200).json({ success: true, userData: doc })
+        }
+    })
+})
+
+app.post('/api/user/login', (req, res) => {
+
+    User.findOne({ email: req.body.email }, (err, user) => {
+        // find email
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "Auth Failed, no such email exists"
+            })
+        }
+
+        // compare password
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "Auth Failed, wrong password"
+                })
+            }
+        })
+
+        user.generateTokens((err, user) => {
+            if (err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true
+                })
+        })
+    })
+})
+
+app.get('/api/user/logout', auth, (req, res) => {
+    User.findOneAndUpdate({_id: req.user._id}, {token: ""}, (err, doc) => {
+        if (err) return res.json({logoutSuccess: false, err});
+        return res.status(200).json({logoutSuccess: true});
+    })
+})
+
+app.get('/api/business/', (req, res) => {
+    Business.find({})
+        .then(business => {
+            res.json(business)
+        })
+})
+
+app.post('/api/business/register', (req, res) => {
+    const business = new Business(req.body);
+
+    business.save((err, doc) => {
+        if (err) {
+            return res.json({ success: false, err })
+        } else {
+            return res.status(200).json({ success: true, businessData: doc })
+        }
+    })
+})
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ models/User.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    preference: {
+        type: String
+    },
+    token: {
+        type: String
+    }
+});
+
+UserSchema.pre('save', function(next) {
+    const user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+})
+
+UserSchema.methods.comparePassword = function (plainPassword, callback) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if (err) return callback(err);
+        callback(null, isMatch)
+    })
+}
+
+UserSchema.methods.generateTokens = function (cb) {
+    const user = this;
+    const token = jwt.sign(user._id.toHexString(), 'secret')
+    user.token = token;
+    user.save( (err, user) => {
+        if (err) return cb(err);
+        cb(null, user); 
+    })
+} 
+
+UserSchema.statics.findByToken = function (token, cb) {
+    const users = this;
+    console.log(users);
+    
+    jwt.verify(token, 'secret', function (err, decode) {
+        users.findOne({"_id": decode, "token": token}, (err, user) => { //plz don't use .then
+            if(err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
+
+module.exports = User = mongoose.model("User", UserSchema);
